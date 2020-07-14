d6d978282846113acaa63a421079e5fe113de70d
Jonathan McCaffrey <jonmcc.0723@gmail.com>
2020-07-14 16:27:24 -0400

    Fixing issues

--- POEHideoutGround/Components/TileCalculatorBase.cs
+++ POEHideoutGround/Components/TileCalculatorBase.cs
@@ -1,22 +1,18 @@
-﻿using Data;
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
 using POEHideoutGround.Shared;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace POEHideoutGround.Components
 {
     public class TileCalculatorBase : ComponentBase
     {
-        int SelectedKey = 0;
+        public int SelectedKey = 0;
 
         [Parameter]
         public TileData DefaultTileData { get; set; }
 
         [Parameter]
-        public TileData[] TileDataList { get; set; }
+        public IEnumerable<TileData> TileData { get; set; }
 
 
         [Parameter]
@@ -24,5 +20,10 @@ namespace POEHideoutGround.Components
 
         [Parameter]
         public bool DefaultTile { get; set; }
+
+        public void Toggle(object checkedValue)
+        {
+
+        }
     }
 }
--- POEHideoutGround/Shared/TileData.cs
+++ POEHideoutGround/Shared/TileData.cs
@@ -18,6 +18,50 @@ namespace POEHideoutGround.Shared
         /// </summary>
         public string Hash { get; set; }
 
-     
+        /// <summary>
+        /// Unique variant/hash key used by the calculator.
+        /// </summary>
+        public int Key
+        {
+            get { return int.Parse(Hash) * 100 + int.Parse(Var); }
+        }
+
+
+        /// <summary>
+        /// The variant displayed to the player in-game. Which is the variant number + 1
+        ///</summary>
+        public string DisplayVar { get { return (int.Parse(Var) + 1).ToString(); } }
+
+
+        #region unused
+        /// <summary>
+        /// Asset instance X position in the hideout
+        /// </summary>
+        public int X { get; set; }
+
+        /// <summary>
+        /// Asset instance Y position in the hideout
+        ///</summary>
+        public int Y { get; set; }
+
+        /// <summary>
+        /// Asset instance rotation in the hideout. 90 degrees represented by the value 16385.
+        /// </summary>
+        public int Rot { get; set; }
+
+        /// <summary>
+        /// Asset instance mirroring in the hideout. Probably mirrored on the X-axis.
+        /// </summary>
+        public bool Flip { get; set; }
+        #endregion
+
+        public TileData() { }
+
+        public TileData(string name, string var, string hash)
+        {
+            Name = name;
+            Var = var;
+            Hash = hash;
+        }
     }
 }
