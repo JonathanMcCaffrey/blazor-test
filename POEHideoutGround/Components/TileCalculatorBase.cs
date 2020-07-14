cb5e56e32d69ed1f957f1dafa8f468ddbc1da0b2
Jonathan McCaffrey <jonmcc.0723@gmail.com>
2020-07-14 16:06:50 -0400

    Testing Blazor parameter issue with classes

---
+++ POEHideoutGround/Components/TileCalculatorBase.cs
@@ -0,0 +1,25 @@
+﻿using Data;
+using Microsoft.AspNetCore.Components;
+using POEHideoutGround.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POEHideoutGround.Components
+{
+    public class TileCalculatorBase : ComponentBase
+    {
+
+
+        [Parameter]
+        public bool IsDisabled { get; set; }
+
+        [Parameter]
+        public Class1 Test;
+
+        
+        [Parameter]
+        public bool DefaultTile { get; set; }
+    }
+}
--- POEHideoutGround/Shared/TileData.cs
+++ POEHideoutGround/Shared/TileData.cs
@@ -1,78 +1,23 @@
-﻿using Microsoft.AspNetCore.Components;
-using System;
-
-//TODO Can't make new namespaces. VS Studio or Blazor bug?
-//namespace POEHideoutGround.Data
+﻿
 namespace POEHideoutGround.Shared
 {
-    public class TileData : ComponentBase
+    public class TileData
     {
         /// <summary>
         /// Human readable asset name.
         /// </summary>
-        [Parameter]
         public string Name { get; set; }
 
         /// <summary>
         /// Display variant used of asset.
         /// </summary>
-        [Parameter]
         public string Var { get; set; }
 
         /// <summary>
         /// Identifiction hash of the static asset reference.
         /// </summary>
-        [Parameter]
         public string Hash { get; set; }
 
-        /// <summary>
-        /// Unique variant/hash key used by the calculator.
-        /// </summary>
-        public int Key()
-        {
-            return int.Parse(Hash) * 100 + int.Parse(Var);
-        }
-       
-
-        /// <summary>
-        /// The variant displayed to the player in-game. Which is the variant number + 1
-        ///</summary>
-        public string DisplayVar { get { return (int.Parse(Var) + 1).ToString(); } }
-
-
-        #region unused
-        /// <summary>
-        /// Asset instance X position in the hideout
-        /// </summary>
-        [Parameter]
-        public int X { get; set; }
-
-        /// <summary>
-        /// Asset instance Y position in the hideout
-        ///</summary>
-        [Parameter]
-        public int Y { get; set; }
-
-        /// <summary>
-        /// Asset instance rotation in the hideout. 90 degrees represented by the value 16385.
-        /// </summary>
-        [Parameter]
-        public int Rot { get; set; }
-
-        /// <summary>
-        /// Asset instance mirroring in the hideout. Probably mirrored on the X-axis.
-        /// </summary>
-        [Parameter]
-        public bool Flip { get; set; }
-        #endregion
-
-        public TileData() { }
-
-        public TileData(string name, string var, string hash)
-        {
-            Name = name;
-            Var = var;
-            Hash = hash;
-        }
+     
     }
 }
