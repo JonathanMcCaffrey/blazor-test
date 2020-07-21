9ea25408ce479fa7b387453e5187e031c43ed388
Jonathan McCaffrey <jonmcc.0723@gmail.com>
2020-07-21 08:55:20 -0400

    WIP

---
+++ POEHideoutGround/Pages/Painter/PaintTile/PaintTileBase.cs
@@ -0,0 +1,109 @@
+﻿using BlazorStyled;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using POEHideoutGround.Data;
+
+namespace POEHideoutGround.Pages.Painter.PaintTile
+{
+    public class PaintTileBase : ComponentBase
+    {
+
+        [Inject]
+        private IStyled Styled { get; set; }
+
+
+        [Parameter]
+        public int YPosition { get; set; } = 0;
+
+        [Parameter]
+        public int XPosition { get; set; } = 0;
+
+        [Parameter]
+        public bool IsPainting { get; set; } = false;
+
+
+        public string Label { get; set; } = "";
+
+
+        protected string TileColour { get; set; } = "purple";
+
+
+        public string TileImage { get; set; } = "arctic_2_l";
+
+        protected string painttile_Container;
+        protected string painttile_Position;
+        protected string painttile_Highlight;
+        protected string painttile_Image;
+
+        [Parameter]
+        public TileData SelectedBrush { get; set; }
+
+        protected TileData PaintData { get; set; }
+
+
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+
+            //painttile_Container;
+
+            Label = $"{XPosition}:{YPosition}";
+
+
+            painttile_Position = Styled.Css($@"
+                top:{XPosition * 23}px;
+                left:{YPosition * 23}px;
+            ");
+
+
+        }
+
+        protected void OnClick(MouseEventArgs e)
+        {
+            TileImage = SelectedBrush.Image;
+
+            PaintData = SelectedBrush;
+
+            painttile_Image = Styled.Css($@"
+                background-image: url('images/tile/{TileImage}.png');
+            ");
+
+        }
+
+
+        protected void OnMouseOver(MouseEventArgs e)
+        {
+
+
+            TileColour = "red";
+
+            painttile_Highlight= Styled.Css($@"
+                border: 2px solid {TileColour};
+            ");
+
+            if(IsPainting)
+            {
+                OnClick(e);
+            }
+
+
+            //e.target.style.color = "orange";
+
+        }
+
+
+        protected void OnMouseOut(MouseEventArgs e)
+        {
+            TileColour = "green";
+
+            painttile_Highlight = Styled.Css($@"
+                border: 2px solid {TileColour};
+            ");
+
+
+        }
+    }
+}
+
---
+++ POEHideoutGround/Pages/Painter/PainterBase.cs
@@ -0,0 +1,93 @@
+﻿using MatBlazor;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.JSInterop;
+using POEHideoutGround.Components;
+using POEHideoutGround.Data;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace POEHideoutGround.Pages.Painter
+{
+    public class PainterBase : ComponentBase
+    {
+
+        [Inject]
+        private HttpClient Http { get; set; }
+
+
+        int minX = 153;
+        int minY = 559;
+        int maxX = 560;
+        int maxY = 153;
+
+        int minTileSize = 23;
+
+        int tileCanvasWidth = 17;
+        int tileCanvasHeight = 17;
+
+        List<TileData> canvasData;
+
+        protected int BrushSize { get; set; }
+
+
+        protected string SelectedBrushKey { get; set; } = "2825914735_1";
+
+
+        public static TileData DefaultBrush = new TileData(name: "Arctic", var: "1", hash: "2825914735", image: "arctic_2_l", size: 69);
+
+        public TileData SelectedBrush
+        {
+            get
+            {
+
+                if (BrushData == null) return DefaultBrush;
+
+                foreach (var tile in BrushData)
+                {
+                    if (tile.Key == SelectedBrushKey)
+                    {
+                        return tile;
+                    }
+                }
+
+                // Should never be null
+                return null;
+            }
+
+            set { }
+        }
+
+
+        protected List<TileData> BrushData;
+
+        protected override async Task OnInitializedAsync()
+        {
+
+            BrushData = await Http.GetJsonAsync<List<TileData>>("data/ground.json");
+        }
+
+
+        protected bool IsPainting { get; set; } = false;
+        protected void OnMouseDown(MouseEventArgs mouseEventArgs)
+        {
+            IsPainting = true;
+        }
+
+        protected void OnMouseUp(MouseEventArgs mouseEventArgs)
+        {
+            IsPainting = false;
+        }
+
+        protected void OnMouseOut(MouseEventArgs mouseEventArgs)
+        {
+            //TODO Better implementation
+            OnMouseUp(mouseEventArgs);
+        }
+
+
+    }
+}
+
