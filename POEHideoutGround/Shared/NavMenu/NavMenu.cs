78cbb3d5321e48542d49f0708da9ab91aac2cbd9
Jonathan McCaffrey <jonmcc.0723@gmail.com>
2020-08-22 20:25:22 -0400

    Weird JSON crash fix

--- POEHideoutGround/Shared/NavMenu/NavMenu.cs
+++ POEHideoutGround/Shared/NavMenu/NavMenu.cs
@@ -2,37 +2,44 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using MatBlazor;
 using Microsoft.AspNetCore.Components;
+using Newtonsoft.Json;
 using POEHideoutGround.Data.Navbar;
 
 namespace POEHideoutGround.Shared.NavMenu
 {
-  public partial class NavMenu : ComponentBase
-  {
-
-    [Inject]
-    private HttpClient Http { get; set; }
-
-    private bool collapseNavMenu = true;
-
-    public string versionNumber = "0.3.0";
-
-    protected string NavMenuCssClass => collapseNavMenu ? "collapse" : null;
-
-    protected void ToggleNavMenu()
+    public partial class NavMenu : ComponentBase
     {
-      collapseNavMenu = !collapseNavMenu;
+
+        [Inject]
+        private HttpClient Http { get; set; }
+
+        private bool collapseNavMenu = true;
+
+        public string versionNumber = "0.3.0";
+
+        protected string NavMenuCssClass => collapseNavMenu ? "collapse" : null;
+
+        protected void ToggleNavMenu()
+        {
+            collapseNavMenu = !collapseNavMenu;
+        }
+
+
+
+        public Navbar Navbar { get; set; }
+
+
+
+        protected override async Task OnInitializedAsync()
+        {
+
+            // Why is this no longer working...
+            //Navbar  = await Http.GetJsonAsync<Navbar>("data/navbar/data.json");
+
+
+            // Why does this work???
+            var text = await Http.GetStringAsync("data/navbar/data.json");
+            Navbar = JsonConvert.DeserializeObject<Navbar>(text);
+        }
     }
-
-
-
-    public Navbar Navbar { get; set; }
-
-    protected override async Task OnInitializedAsync()
-    {
-      Navbar = await Http.GetJsonAsync<Navbar>("data/navbar/data.json");
-    }
-
-
-  }
-
 }
