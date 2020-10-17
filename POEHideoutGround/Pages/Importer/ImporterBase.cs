b41ce48efe39025e661b50660fe808169274dd04
Jonathan McCaffrey <jonmcc.0723@gmail.com>
2020-10-17 09:39:38 -0400

    Downgrading Blazor version

--- POEHideoutGround/Pages/Importer/ImporterBase.cs
+++ POEHideoutGround/Pages/Importer/ImporterBase.cs
@@ -1,38 +1,11 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
-using BlazorInputFile;
 
 namespace POEHideoutGround.Pages.Importer
 {
   public class ImporterBase : ComponentBase
   {
     public string UploadedDataHideoutFormat { get; set; }
-
-    protected async Task HandleFileSelected(IFileListEntry[] files)
-    {
-      if (files == null || files.Length == 0)
-      {
-        return;
-      }
-
-      var file = files[0];
-
-      UploadedDataHideoutFormat = "";
-
-      using (var reader = new System.IO.StreamReader(file.Data))
-      {
-        var line = await reader.ReadLineAsync();
-
-        while (line != null)
-        {
-          UploadedDataHideoutFormat += line;
-
-          line = await reader.ReadLineAsync();
-        }
-      }
-    }
-
-
   }
 }
 
